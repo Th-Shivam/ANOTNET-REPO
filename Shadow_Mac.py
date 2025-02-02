a4140ec98821b0315fc2c4de9b83e3db65083830
Th-Shivam <dreamyshivam01@gmail.com>
2025-02-02 15:58:56 +0530

    Mac Changer

--- Shadow_Mac.py
+++ Shadow_Mac.py
@@ -1,4 +1,11 @@
 import subprocess
+import optparse
+
+parser  = optparse.OptionParser() 
+
+parser.add_option("-i" , "--interface" dest="innterface" ,  help = "Use this to set the interface")
+
+parser.parse_args()
 
 interface = input("Enter the interface name for which you want to change the MAC :")
 

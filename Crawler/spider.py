b49c258edc5a2ebf96579bee0854ab102ad57ea1
th-shivam <dreamyshivam01@gmail.com>
2025-12-17 11:18:31 +0530

    Extract all the hyperlinks from the target url

--- Crawler/spider.py
+++ Crawler/spider.py
@@ -1,14 +1,13 @@
 import requests
 import re
 
-def request(url):
-    try:
-        return requests.get("https://" + url)
-    except requests.exceptions.ConnectionError:
-        pass
-
-    
-response = request("zsecurity.org")
-href_links = re.findall('(?:href=")(.*?)"' , response.text )
-print(href_links)     
+def get_links(target_url):        
+    response = requests.get(target_url)
+    return re.findall('(?:href=")(.*?)"' , response.text )
+         
+url = "https://zsecurity.org" 
+href_links = get_links(url)
+for link in href_links:
+    if url in link:
+        print(link)
 
